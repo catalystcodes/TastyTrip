cc179714d590a5cf7294544e4ca9cb9a16fd7a2b
catalystcodes <muh4mub@gmail.com>
2024-11-01 23:24:24 +0100

    feat: profile Page

--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -1,8 +1,20 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
 
 const Profile = () => {
-  return <View></View>;
+  return (
+    <View>
+      <View style={{ marginTop: hp(11), margin: "auto" }}>
+        <Image source={require("../assets/profilePics.png")} />
+      </View>
+      <Text>Itoh</Text>
+      <Text>+1 11229382748</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
