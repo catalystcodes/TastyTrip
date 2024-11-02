ed69c09736698664ff141bfa7969d3bb677c2d39
catalystcodes <muh4mub@gmail.com>
2024-11-02 02:08:36 +0100

    feat: change password page

--- src/screens/changePassword.tsx
+++ src/screens/changePassword.tsx
@@ -1,8 +1,39 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import HeadLine from "../components/molecules/headLine";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
+import AppInputText from "../components/molecules/appInputText";
+import AppButton from "../components/atoms/appButton";
 
 const ChangePassword = () => {
-  return <View></View>;
+  return (
+    <View
+      style={{
+        flexGrow: 1,
+        paddingHorizontal: wp(7.2),
+        backgroundColor: "white",
+      }}
+    >
+      <View style={{ marginTop: hp(8.8) }}>
+        <HeadLine title="Change Password" />
+      </View>
+      <View>
+        <Text>Enter Old Password</Text>
+        <AppInputText placeholder="Password" />
+      </View>
+      <View>
+        <Text>Enter Old Password</Text>
+        <AppInputText placeholder="Enter New Password" />
+        <AppInputText placeholder="Re-enter New Password" />
+      </View>
+      <View>
+        <AppButton />
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
