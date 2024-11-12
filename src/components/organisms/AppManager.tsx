93e4cf4258c45b4c5a946a10b2d33e378e530cc9
catalystcodes <muh4mub@gmail.com>
2024-11-12 13:51:11 +0100

    feat: settingUpReduxToolkit

--- src/components/organisms/AppManager.tsx
+++ src/components/organisms/AppManager.tsx
@@ -3,16 +3,20 @@ import React, { ReactNode } from "react";
 import { StatusBar, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { store } from "../../../store";
+import { Provider } from "react-redux";
 
 const AppManager = ({ children }: { children: ReactNode }) => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <StatusBar backgroundColor="black" />
-          {children}
-        </NavigationContainer>
-      </SafeAreaProvider>
+      <Provider store={store}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <StatusBar backgroundColor="black" />
+            {children}
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </Provider>
     </GestureHandlerRootView>
   );
 };
