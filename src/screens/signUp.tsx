55dfca58288d7ae018bc3ddff778068222abff70
catalystcodes <muh4mub@gmail.com>
2024-12-16 16:03:33 +0100

    update database

--- src/screens/signUp.tsx
+++ src/screens/signUp.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
 import HeadLine from "../components/molecules/headLine";
 import {
   heightPercentageToDP as hp,
@@ -10,15 +10,45 @@ import KeyboardAvoidView from "../components/organisms/keyboardAvoidView";
 import AppButton from "../components/atoms/appButton";
 import ConnectWith from "../components/atoms/connectWith";
 import GoogleAndFacebookConnect from "../components/molecules/googleAndFacebookConnect";
+import AuthFooter from "../components/molecules/AuthFooter";
+import { addNewUser } from "../utils/auth.helper";
+
+const defaultForm = {
+  username: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const SignUp = ({ navigation }: any) => {
+  const [form, setForm] = useState(defaultForm);
+
+  const disableButton =
+    form.username?.trim() === "" ||
+    form.password?.trim() === "" ||
+    form.confirmPassword?.trim() === "";
+
+  const handleSignUp = async () => {
+    if (disableButton) return;
+    if (form.password !== form.confirmPassword) {
+      Alert.alert("Passwords do not match");
+      return;
+    }
+    const { confirmPassword, ...otherFormFields } = form;
+    try {
+      const data = await addNewUser(otherFormFields);
+      if (data) {
+        navigation.navigate("loginPage");
+      }
+    } catch (e) {}
+  };
+
   return (
     <View style={{ flexGrow: 1, backgroundColor: "white" }}>
       <KeyboardAvoidView>
         <View style={{ paddingHorizontal: wp(7.2) }}>
           <View
             style={{
-              marginTop: hp(8.8),
+              marginTop: hp(3.34),
               marginBottom: hp(17.1),
             }}
           >
@@ -31,19 +61,28 @@ const SignUp = ({ navigation }: any) => {
           </Text>
           <AppInputText
             placeholder="Enter Username"
+            value={form.username}
+            onChangeText={(text) => setForm({ ...form, username: text })}
             style={{ marginBottom: hp(2.2) }}
           />
           <AppInputText
             placeholder="Enter Password"
+            value={form.password}
+            type="password"
+            onChangeText={(text) => setForm({ ...form, password: text })}
             style={{ marginBottom: hp(3.3) }}
           />
           <AppInputText
             placeholder="Re-enter Password"
+            type="password"
+            value={form.confirmPassword}
+            onChangeText={(text) => setForm({ ...form, confirmPassword: text })}
             style={{ marginBottom: hp(3.3) }}
           />
           <AppButton
             text="Sign Up"
-            onPress={() => navigation.navigate("appBottomTab")}
+            onPress={handleSignUp}
+            disabled={disableButton}
           />
           <Text
             style={{
@@ -55,27 +94,7 @@ const SignUp = ({ navigation }: any) => {
             Forgot Password?
           </Text>
         </View>
-        <ConnectWith />
-
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            paddingRight: wp(7.2),
-          }}
-        >
-          <Image
-            source={require("../assets/PngItem_39514 1.png")}
-            style={styles.foodImage}
-          />
-          <View
-            style={{
-              marginTop: hp(2.2),
-            }}
-          >
-            <GoogleAndFacebookConnect />
-          </View>
-        </View>
+        <AuthFooter />
       </KeyboardAvoidView>
     </View>
   );
