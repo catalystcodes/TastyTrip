94d2b45a92ad2718b613fa71a3193abc6d05b0cb
catalystcodes <muh4mub@gmail.com>
2025-01-09 16:28:29 +0100

    update data base

--- src/screens/myProfile.tsx
+++ src/screens/myProfile.tsx
@@ -1,8 +1,36 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { baseUrl } from "../utils/config";
+import axios from "axios";
+import { useAuthUser } from "../utils/common.hooks";
 
 const MyProfile = () => {
-  return <View></View>;
+  const [userDetails, setUserDetails] = useState({});
+  const { token } = useAuthUser();
+
+  useEffect(() => {
+    const getProfileData = async () => {
+      try {
+        const { data } = await axios(`${baseUrl}/auth/me`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        console.log({ data });
+        setUserDetails(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProfileData();
+  }, []);
+
+  return (
+    <View>
+      <Text>My Profile </Text>
+      <Text>{userDetails.firstName}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
